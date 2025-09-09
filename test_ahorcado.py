45e8996c00a163e62be5c6eb13e56143ff3fcd4f
lauratulian <lau.tulian01@gmail.com>
2025-09-09 11:56:27 -0300

    Test 2

--- test_ahorcado.py
+++ test_ahorcado.py
@@ -1,8 +1,15 @@
 import pytest
 from ahorcado import JuegoAhorcado
 
-def test_adivino_letra_que_esta_en_palabra():
+def test_adivino_letra_en_palabra():
     juego = JuegoAhorcado("python")
     resultado = juego.adivinar_letra("p")
     assert resultado is True
     assert "p" in juego.letras_acertadas
+
+def test_adivino_letra_no_en_palabra():
+    juego = JuegoAhorcado("python")
+    juego.vidas = 6
+    resultado = juego.adivinar_letra("z")
+    assert resultado is False
+    assert "z" in juego.letras_erroneas
