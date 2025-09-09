08062fe3d332f510d237ac467c9d46dfa9cdf50a
lauratulian <lau.tulian01@gmail.com>
2025-09-09 12:50:54 -0300

    Test 5: Funcionando

--- ahorcado.py
+++ ahorcado.py
@@ -3,6 +3,7 @@ import random
 class JuegoAhorcado:
     def __init__(self, palabra):
         self.palabra = palabra
+        self.vidas = 6
         self.letras_acertadas = []
         self.letras_erroneas = []
 
@@ -12,10 +13,12 @@ class JuegoAhorcado:
             return True
         else:
             self.letras_erroneas.append(letra)
+            self.vidas -= 1
             return False
         
     def adivinar_palabra(self, intento):
         if intento == self.palabra:
             return True
         else:
+            self.vidas -= 1
             return False  
