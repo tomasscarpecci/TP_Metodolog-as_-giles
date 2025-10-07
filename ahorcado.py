074a26e6c739f0e7a4917915ebb70e75d5726f1c
tomasscarpecci <166225240+tomasscarpecci@users.noreply.github.com>
2025-10-06 21:25:06 -0300

    Corrijo sintaxis

--- ahorcado.py
+++ ahorcado.py
@@ -2,8 +2,9 @@ import random
 import string
 from wordfreq import top_n_list
 
+
 class JuegoAhorcado:
-    def __init__(self, palabra):  
+    def __init__(self, palabra):
         self.palabra = palabra
         self.vidas = 6
         self.letras_acertadas = []
@@ -14,14 +15,13 @@ class JuegoAhorcado:
     def reiniciar_con_palabra(self, nueva_palabra):
         if " " in nueva_palabra:
             raise ValueError("La palabra no puede contener espacios.")
-        
+
         self.palabra = nueva_palabra.lower()
         self.vidas = 6
         self.letras_acertadas = []
         self.letras_erroneas = []
         self.ganado = False
 
-    
     def seleccionar_palabra_wordfreq(idioma="es", cantidad=50):
         lista_palabras = top_n_list(idioma, cantidad)
         if not lista_palabras:
@@ -29,18 +29,18 @@ class JuegoAhorcado:
         return random.choice(lista_palabras)
 
     def seleccionar_palabra_aleatoria(lista_palabras):
-        if not lista_palabras: 
+        if not lista_palabras:
             raise ValueError("La lista de palabras no puede estar vacía.")
         return random.choice(lista_palabras)
 
     def adivinar_letra(self, letra):
-        
+
         if self.esta_terminado():
             raise RuntimeError("El juego ya terminó.")
-        
+
         if not self.validar_letra(letra):
             raise ValueError("La letra debe ser un caracter alfabético único.")
-        
+
         if letra in self.palabra:
             if letra not in self.letras_acertadas:
                 self.letras_acertadas.append(letra)
@@ -52,41 +52,41 @@ class JuegoAhorcado:
                 self.letras_erroneas.append(letra)
                 self.quitar_vida()
             return False
-        
+
     def adivinar_palabra(self, intento):
         if self.terminado:
             raise RuntimeError("El juego ya terminó.")
-        
+
         self.validar_palabra(intento)
-    
+
         if intento == self.palabra:
             return True
         else:
             self.quitar_vida()
-            return False  
-        
+            return False
+
     def mostrar_letras_acertadas(self):
         return self.letras_acertadas
-    
+
     def mostrar_letras_erroneas(self):
         return self.letras_erroneas
-    
+
     def esta_ganado(self):
         return self.ganado
-   
+
     def esta_derrotado(self):
         return self.vidas <= 0
-    
+
     def quitar_vida(self):
-        if self.vidas > 0:        
+        if self.vidas > 0:
             self.vidas -= 1
 
     def validar_letra(self, letra):
         return len(letra) == 1 and letra in string.ascii_letters
-    
+
     def esta_terminado(self):
         return self.esta_derrotado() or self.esta_ganado()
-    
+
     def validar_palabra(self, palabra):
         if not palabra:
             raise ValueError("La palabra no puede estar vacía.")
@@ -95,5 +95,3 @@ class JuegoAhorcado:
         if not all(c in string.ascii_letters for c in palabra):
             raise ValueError("La palabra solo puede contener letras.")
         return True
-    
-    
